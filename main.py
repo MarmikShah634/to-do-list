ad2854547f17b0a7f034f66abcb0678e3880bbd5
Vraj Shah <210130107001@gecg28.ac.in>
2024-01-10 13:47:22 +0530

    m

---
+++ main.py
@@ -0,0 +1,23 @@
+import json
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+with open('flaskenv/config.json', 'r') as c:
+    params = json.load(c)['params']
+
+app =Flask(__name__,template_folder='templates')
+# app.secret_key = 'super-secret-key'
+local_server = True
+
+if local_server:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+    
+db = SQLAlchemy(app)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+app.run(debug=True)
